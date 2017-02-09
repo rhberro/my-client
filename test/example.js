a6273411415972f056ef462745f8e45ce4925ea5
Rafael Henrique Berro <rhberro@gmail.com>
2017-02-08 23:12:27 -0200

    Remove unnecessary example of test.

--- test/example.js
+++ test/example.js
@@ -11,15 +11,3 @@ describe('Example Group 1', function () {
     })
   })
 })
-
-describe('Example Group 2', function () {
-  describe('Example Task 2', function () {
-    it('Should check the positive numbers equality.', function () {
-      assert.equal(2, 2)
-    })
-
-    it('Should check the negative numbers equality.', function () {
-      assert.equal(-2, -2)
-    })
-  })
-})
