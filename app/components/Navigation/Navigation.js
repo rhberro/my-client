ba12e89b4cfa9ce7b7f5bc456d0f2b905150164c
Rafael Henrique Berro <rhberro@gmail.com>
2017-02-28 21:17:54 -0300

    Add a external link to the github repository in the navigation component.

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -15,6 +15,12 @@ const Navigation = () => {
           <Link to='about' className='nav__anchor' activeClassName='nav__anchor--active'>About</Link>
         </li>
       </ul>
+
+      <ul className='nav nav--right'>
+        <li className='nav__item'>
+          <a className='nav__anchor' href='https://github.com/rhberro/the-react-client'>Project</a>
+        </li>
+      </ul>
     </nav>
   )
 }
