1de81502b04c63dc02a420d05f679aeb8151b2d3
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-07 02:28:29 -0300

    Add the jsx extension to the navbar component.

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+import './Navbar.scss'
+
+const Navbar = () => (
+  <nav className='nav'>
+    <ul className='nav__container nav__container--left'>
+      <li className='nav__item'>
+        <Link to='/' className='nav__anchor' activeClassName='nav__anchor--active'>
+          Home
+        </Link>
+      </li>
+
+      <li className='nav__item'>
+        <Link to='about' className='nav__anchor' activeClassName='nav__anchor--active'>
+          About
+        </Link>
+      </li>
+    </ul>
+
+    <ul className='nav__container nav__container--right'>
+      <li className='nav__item'>
+        <Link to='/' className='nav__anchor' activeClassName='nav__anchor--active'>
+          Home
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Navbar
