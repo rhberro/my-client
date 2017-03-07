d850ded86d316246a584960a018e87ed0f743da8
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-07 02:28:19 -0300

    Add the jsx extension to the footer component.

---
+++ app/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.scss'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a className='footer__anchor' href='https://github.com/rhberro/the-react-client'>
+      Github
+    </a>
+  </footer>
+)
+
+export default Footer
