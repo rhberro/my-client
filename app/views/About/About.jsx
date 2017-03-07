ea6496eb571cff8702a2007fee6d3b6568fc59c0
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-07 02:28:42 -0300

    Add the jsx extension to the about view.

---
+++ app/views/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './About.scss'
+
+const About = () => (
+  <div className='about'>
+    <h2 className='about__title'>This is the about page.</h2>
+  </div>
+)
+
+export default About
