96b17075981a47c60d1377dcb4343825cfb159d3
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-22 23:29:04 -0300

    Add a test to the store.

--- test/store.test.js
+++ test/store.test.js
@@ -1,15 +1,19 @@
-const store = require('../app/store').default
+import store from '../app/store'
 
-test( 'the state tree should match', () => {
-  const currentStates = store.getState()
-  const desiredStates = { tasks : {} }
-
-  expect( currentStates ).toMatchObject( desiredStates )
+/* eslint-disable */
+describe( 'the state tree', () => {
+  it( 'should match', () => {
+    const currentStates = store.getState()
+    const desiredStates = { tasks : {} }
+    expect( currentStates ).toMatchObject( desiredStates )
+  } )
 } )
 
-test( 'the state tasks should match', () => {
-  const currentState = store.getState().tasks
-  const desiredStates = { payload: [], fetching: false, success: false, error: false }
-
-  expect( currentState ).toMatchObject( desiredStates )
+describe( 'the state tasks', () => {
+  it( 'should match', () => {
+    const currentState = store.getState().tasks
+    const desiredStates = { payload: [], fetching: false, success: false, error: false }
+    expect( currentState ).toMatchObject( desiredStates )
+  } )
 } )
+/* eslint-enable */
