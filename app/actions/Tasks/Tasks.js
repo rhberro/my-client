072968babe24f9670250c5f0e2e499b047878611
Rafael Henrique Berro <rhberro@gmail.com>
2017-03-22 23:29:24 -0300

    Add the missing actions to the tasks state.

--- app/actions/Tasks/Tasks.js
+++ app/actions/Tasks/Tasks.js
@@ -1,5 +1,18 @@
-export const getTasks = () => (
+export const tasks = () => (
   {
     type: 'TASKS'
+  }
+)
+
+export const tasksFailure = () => (
+  {
+    type: 'TASKS_FAILURE'
+  }
+)
+
+export const tasksSuccess = (payload) => (
+  {
+    type: 'TASKS_SUCCESS',
+    payload
   }
 )
