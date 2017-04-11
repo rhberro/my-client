e22bfdaec47a410a3b4defbc7529147f392df7e5
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-10 23:26:43 -0300

    Keep it simple.

--- app/views/Layout/Layout.jsx
+++ app/views/Layout/Layout.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-
 import './Layout.scss'
 
 const Layout = props => (
   <div className='layout'>
     <div className='layout__container'>
-      <Header />{ props.children }
-      <Footer />
+      { props.children }
     </div>
   </div>
 )
