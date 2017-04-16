b0a36d1e73fd24c9bcfbb48514384931a7c15dc0
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-16 15:30:15 -0300

    Add an example of component.

---
+++ app/components/User/User.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { getUser } from '../../actions/User'
+
+class User extends React.Component {
+  componentWillMount () {
+    this._getUser()
+  }
+
+  _getUser () {
+    const { getUser } = this.props
+    getUser()
+  }
+
+  _renderLoading () {
+    return (
+      <div>Status: Loading</div>
+    )
+  }
+
+  _renderError () {
+    return (
+      <div>Status: Error</div>
+    )
+  }
+
+  _renderSuccess () {
+    // return <div>{ this.props.user.payload.login }</div>
+    return <div>Status: Success</div>
+  }
+
+  _renderPlaceholder () {
+    return <div>Status: Placeholder</div>
+  }
+
+  render () {
+    const { user: { fetching, error, success } } = this.props
+    if (fetching) return this._renderLoading()
+    if (error) return this._renderError()
+    if (success) return this._renderSuccess()
+    return this._renderPlaceholder()
+  }
+}
+
+const mapStateToProps = state => {
+  const { user } = state
+  return { user }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  { getUser },
+  dispatch
+)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(User)
