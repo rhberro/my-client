0b1b6cea53513a3b4f1622fe8eac018d883381a0
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-18 22:13:58 -0300

    Add the webpack configuration files for two environments.

---
+++ webpack.development.config.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const webpack = require('webpack')
+const path = require('path')
+
+const configuration = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'app')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: '8080'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'react-hot-loader', include: path.join(__dirname, 'app') },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: 'eval',
+  cache: false
+}
+
+module.exports = configuration
