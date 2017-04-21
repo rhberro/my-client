136b4504ba5ce4c178c6607e7a443b61c2665f65
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-20 23:04:08 -0300

    Remove all of the jsx extension on files.

--- app/components/User/User.js
+++ app/components/User/User.js
@@ -5,37 +5,25 @@ import { connect } from 'react-redux'
 import { getUser } from '../../actions/User'
 
 class User extends React.Component {
+  constructor () {
+    super()
+    this._renderLoading = <div>Status: Loading</div>
+    this._renderError = <div>Status: Error</div>
+    this._renderSuccess = <div>Status: Success</div>
+    this._renderPlaceholder = <div>Status: Placeholder</div>
+  }
+
   componentWillMount () {
     const { getUser } = this.props
     getUser()
   }
 
-  _renderLoading () {
-    return (
-      <div>Status: Loading</div>
-    )
-  }
-
-  _renderError () {
-    return (
-      <div>Status: Error</div>
-    )
-  }
-
-  _renderSuccess () {
-    return <div>Status: Success</div>
-  }
-
-  _renderPlaceholder () {
-    return <div>Status: Placeholder</div>
-  }
-
   render () {
     const { user: { fetching, error, success } } = this.props
-    if (fetching) return this._renderLoading()
-    if (error) return this._renderError()
-    if (success) return this._renderSuccess()
-    return this._renderPlaceholder()
+    if (fetching) return this._renderLoading
+    if (error) return this._renderError
+    if (success) return this._renderSuccess
+    return this._renderPlaceholder
   }
 }
 
