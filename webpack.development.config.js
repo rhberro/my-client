ac34993a5baa21d573fcbf3c6da79dbfd3aaa693
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-21 20:45:01 -0300

    Add a new build process for the development environment.

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -3,6 +3,8 @@
 const webpack = require('webpack')
 const path = require('path')
 
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const configuration = {
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:8080',
@@ -10,21 +12,14 @@ const configuration = {
     path.resolve(__dirname, 'app')
   ],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    historyApiFallback: true,
-    host: '0.0.0.0',
-    port: '8080',
-    hot: true
+    path: path.resolve(__dirname, 'public/build/'),
+    filename: '[name].js'
   },
   module: {
     loaders: [
-      { test: /\.js?$/, loader: 'react-hot-loader', include: path.join(__dirname, 'app') },
       { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+      { test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.html$/, loaders: [ 'html-loader' ] }
     ]
   },
   plugins: [
@@ -33,11 +28,27 @@ const configuration = {
         'process.env.NODE_ENV': JSON.stringify('development')
       }
     ),
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, 'public/index.html'),
+        favicon: path.resolve(__dirname, 'public/favicon.ico'),
+        inject: true,
+        minify: false
+      }
+    ),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin()
   ],
   devtool: 'eval',
-  cache: false
+  cache: false,
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: '8080',
+    hot: true
+  }
 }
 
 module.exports = configuration
