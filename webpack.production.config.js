caf0a2289145c699a853aa75449bbaebacb0b7e9
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-21 20:45:07 -0300

    Add a new build process for the production environment.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,23 +3,35 @@
 const webpack = require('webpack')
 const path = require('path')
 
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const configuration = {
-  entry: path.resolve(__dirname, 'app'),
+  entry: [
+    path.resolve(__dirname, 'app')
+  ],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'public/build/'),
+    filename: '[hash].js'
   },
   module: {
     loaders: [
-      { test: /\.js?$/, loader: 'react-hot-loader', include: path.join(__dirname, 'app') },
       { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+      { test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.html$/, loaders: [ 'html-loader' ] }
     ]
   },
   plugins: [
     new webpack.DefinePlugin(
       {
         'process.env.NODE_ENV': JSON.stringify('production')
+      }
+    ),
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, 'public/index.html'),
+        favicon: path.resolve(__dirname, 'public/favicon.ico'),
+        inject: true,
+        minify: {}
       }
     ),
     new webpack.optimize.AggressiveMergingPlugin(),
