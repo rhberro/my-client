34c1c682a8b9cd21d49c3d1463de563588016634
Rafael Henrique Berro <rhberro@gmail.com>
2017-04-21 22:04:58 -0300

    Adjust the server port.

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
 
 app.use(
   express.static(
-    path.join(__dirname, '/public/')
+    path.join(__dirname, '/public/build/')
   )
 )
 
-app.get('*', function (req, res) {
-  res.sendFile(
-    path.join(__dirname, '/public/index.html')
-  )
-})
+app.get(
+  '*', (req, res) => {
+    const index = path.join(__dirname, '/public/build/index.html')
+    res.sendFile(index)
+  }
+)
 
-app.listen(8080, () => console.log('Running server on 8080.'))
+app.listen(
+  process.env.PORT || 8080, error => {
+    if (error) return console.error(error)
+    return console.log('Server running on port ', process.env.PORT || 8080)
+  }
+)
