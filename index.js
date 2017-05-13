dffaba4085080fcd488d5ee2e2b8c053ffabc5de
Rafael Henrique Berro <rhberro@gmail.com>
2017-05-12 23:33:26 -0300

    First attempt to serve files with the gz extension.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const path = require('path')
+
 const app = express()
+
+app.use(
+  (request, response, next) => {
+    const validHeaders = request.headers['accept-encoding'].indexOf('gzip') > -1
+    if (!validHeaders) return next()
+
+    const validFile = /(\.js|\.css)$/.test(request.url)
+    if (!validFile) return next()
+
+    request.url = request.url + '.gz'
+    response.setHeader('Content-Encoding', 'gzip')
+
+    return next()
+  }
+)
 
 app.use(
   express.static(
@@ -10,14 +26,14 @@ app.use(
 
 app.get(
   '*', (req, res) => {
-    const index = path.join(__dirname, '/public/build/index.html')
-    res.sendFile(index)
+    res.sendFile(
+      path.join(__dirname, '/public/build/index.html')
+    )
   }
 )
 
 app.listen(
-  process.env.PORT || 8080, error => {
-    if (error) return console.error(error)
+  process.env.PORT || 8080, () => {
     return console.log('Server running on port ', process.env.PORT || 8080)
   }
 )
