e1ef52a4483b0acca16408142139704324a52894
Rafael Henrique Berro <rhberro@gmail.com>
2017-05-18 22:54:03 -0300

    Remove the gzip strategy until the production build is done.

--- index.js
+++ index.js
@@ -4,21 +4,6 @@ const path = require('path')
 const app = express()
 
 app.use(
-  (request, response, next) => {
-    const validHeaders = request.headers['accept-encoding'].indexOf('gzip') > -1
-    if (!validHeaders) return next()
-
-    const validFile = /(\.js|\.css)$/.test(request.url)
-    if (!validFile) return next()
-
-    request.url = request.url + '.gz'
-    response.setHeader('Content-Encoding', 'gzip')
-
-    return next()
-  }
-)
-
-app.use(
   express.static(
     path.join(__dirname, '/public/build/')
   )
