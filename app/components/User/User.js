4c159f302ae952eaa4bdbf4950c856e5a7754a4c
Rafael Henrique Berro <rhberro@gmail.com>
2017-08-21 22:34:47 -0300

    Refactoring the User component.

--- app/components/User/User.js
+++ app/components/User/User.js
@@ -1,35 +1,10 @@
 import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 
-import { getUser } from './actions/User'
-
-class User extends React.Component {
-  componentWillMount () {
-    const { getUser } = this.props
-    getUser()
-  }
-
-  render () {
-    const { user: { fetching, error, success } } = this.props
-    if (fetching) return <div>Status: Loading</div>
-    if (error) return <div>Status: Error</div>
-    if (success) return <div>Status: Success</div>
-    return <div>Status: Placeholder</div>
-  }
+const User = ({ fetching, error, success }) => {
+  if (fetching) return <span>Status: Loading</span>
+  if (error) return <span>Status: Error</span>
+  if (success) return <span>Status: Success</span>
+  return <span>Status: Placeholder</span>
 }
 
-const mapStateToProps = state => {
-  const { user } = state
-  return { user }
-}
-
-const mapDispatchToProps = dispatch => bindActionCreators(
-  { getUser },
-  dispatch
-)
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(User)
+export default User
